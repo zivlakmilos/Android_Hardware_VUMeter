6e8c509200ac2d9278e4605641bd33f15c383c99
Milos Zivlak <zivlakmilos@gmail.com>
2016-05-31 00:14:12 +0200

    android: dodat action u main menu i funkcija koju on poziva (treba jos telo funkcije da se odradi

--- android/src/net/ddns/zivlakmilos/hardwarevumeter/MainActivity.java
+++ android/src/net/ddns/zivlakmilos/hardwarevumeter/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -26,9 +27,14 @@ public class MainActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_bluetooth) {
+			actionBluetooth_Click();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private void actionBluetooth_Click()
+	{
 	}
 }
