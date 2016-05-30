158f2447fee3569ba1b7816c0ce6ffd121640a47
Milos Zivlak <zivlakmilos@gmail.com>
2016-05-31 00:37:55 +0200

    Kreiran activity za bluetooth settings

---
+++ android/src/net/ddns/zivlakmilos/hardwarevumeter/BluetoothActivity.java
@@ -0,0 +1,13 @@
+package net.ddns.zivlakmilos.hardwarevumeter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BluetoothActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bluetooth);
+	}
+}
--- android/src/net/ddns/zivlakmilos/hardwarevumeter/MainActivity.java
+++ android/src/net/ddns/zivlakmilos/hardwarevumeter/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+
 	}
 	
 	private void actionBluetooth_Click()
