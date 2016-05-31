885bdd18ba3ba9e8ec925d75ccdd553ad3c720c3
Milos Zivlak <zivlakmilos@gmail.com>
2016-05-31 02:29:31 +0200

    Ubacen listview za bluetooth paired devices (za sada samo tetst)

--- android/src/net/ddns/zivlakmilos/hardwarevumeter/BluetoothActivity.java
+++ android/src/net/ddns/zivlakmilos/hardwarevumeter/BluetoothActivity.java
@@ -2,12 +2,22 @@ package net.ddns.zivlakmilos.hardwarevumeter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class BluetoothActivity extends Activity {
+	
+	private String[] pairedDevices = { "ZI", "ZIMobile" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bluetooth);
+		
+		ArrayAdapter<String> adapterPairedDevices =
+				new ArrayAdapter(this, R.layout.bluetooth_listview, pairedDevices);
+		
+		ListView lwPairedDevices = (ListView)findViewById(R.id.lwPairedDevices);
+		lwPairedDevices.setAdapter(adapterPairedDevices);
 	}
 }
