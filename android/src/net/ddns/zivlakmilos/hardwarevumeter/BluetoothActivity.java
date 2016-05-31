a0fb3180ebb5f149b04706f43f752210953a5074
Milos Zivlak <zivlakmilos@gmail.com>
2016-06-01 00:23:51 +0200

    BluetoothActivity: Uradjena nit za otvaranje konekcije, treba je jos 'iskorisiti' i testirati

--- android/src/net/ddns/zivlakmilos/hardwarevumeter/BluetoothActivity.java
+++ android/src/net/ddns/zivlakmilos/hardwarevumeter/BluetoothActivity.java
@@ -1,16 +1,20 @@
 package net.ddns.zivlakmilos.hardwarevumeter;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -18,8 +22,10 @@ import android.widget.Toast;
 public class BluetoothActivity extends Activity {
 	
 	private final int REQUEST_BT_ENABLE			= 101;
+	private BluetoothAdapter m_btAdapter;
 	private BroadcastReceiver m_reciver;
 	private ArrayAdapter<String> m_pairedDevices;
+	private BluetoothSocket m_btSocket;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +37,16 @@ public class BluetoothActivity extends Activity {
 		ListView lwPairedDevices = (ListView)findViewById(R.id.lwPairedDevices);
 		lwPairedDevices.setAdapter(m_pairedDevices);
 		
-		BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-		if(btAdapter == null)
+		m_btAdapter = BluetoothAdapter.getDefaultAdapter();
+		if(m_btAdapter == null)
 			Toast.makeText(getApplication(), "Uredjaj nije podrzan", Toast.LENGTH_LONG).show();
 		
-		if(!btAdapter.isEnabled()) {
+		if(!m_btAdapter.isEnabled()) {
 			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 			startActivityForResult(enableBtIntent, REQUEST_BT_ENABLE);
 		}
 		
-		Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+		Set<BluetoothDevice> pairedDevices = m_btAdapter.getBondedDevices();
 		if(pairedDevices.size() > 0)
 			for(BluetoothDevice device : pairedDevices) {
 				m_pairedDevices.add(device.getName() + "\n" + device.getAddress());
@@ -66,5 +72,49 @@ public class BluetoothActivity extends Activity {
 	protected void onDestroy() {
 		super.onDestroy();
 		unregisterReceiver(m_reciver);
+	}
+	
+	private class ConnectThred extends Thread {
+		
+		public ConnectThred(BluetoothDevice device) {
+			BluetoothSocket tmp = null;
+			
+			try {
+				tmp = device.createRfcommSocketToServiceRecord(UUID.randomUUID());
+			} catch (IOException ex) {
+				Log.println(Log.ERROR, "Bluetooth", "Kreiranje socket-a nije uspelo");
+			}
+			
+			m_btSocket = tmp;
+		}
+		
+		@Override
+		public void run() {
+			m_btAdapter.cancelDiscovery();
+			
+			try {
+				m_btSocket.connect();
+			} catch(IOException ex) {
+				Log.println(Log.ERROR, "Bluetooth", "Konekcija nije uspela");
+				try {
+					m_btSocket.close();
+				} catch (IOException ex2) {}
+				return ;
+			}
+			
+			/*
+			 * TODO:
+			 * 	Kreirati nit za upravljanje konekcijom
+			 */
+		}
+		
+		public void cancel()
+		{
+			try {
+				m_btSocket.close();
+			} catch (IOException ex) {
+				Log.println(Log.WARN, "Bluetooth", "Nema konekcije za zatvaranje");
+			}
+		}
 	}
 }
